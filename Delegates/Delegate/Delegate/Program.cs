bb74f8514b09b40b2ff7f034412d538604261e31
Zeeshan Lalani <zzlalani@gmail.com>
2014-03-20 08:13:10 +0500

    Delegates

---
+++ Delegates/Delegate/Delegate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+
+    delegate int Add(int a, int b);
+
+    class Program
+    {
+        private static Add addDelegate;
+
+        static void Main(string[] args)
+        {
+
+            addDelegate += new Add(addFunction);
+            addDelegate += new Add(addFunction);
+            addDelegate += new Add(addFunction2);
+
+            int ans = -1;
+            if (addDelegate != null)
+                ans = addDelegate(10,20);
+
+            Console.WriteLine(ans);
+            Console.ReadLine();
+        }
+        
+        static int addFunction(int a, int b) {
+
+            return a + b;
+
+        }
+
+        static int addFunction2(int a, int b)
+        {
+
+            return a - b;
+
+        }
+    }
+}
